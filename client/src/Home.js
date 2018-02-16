8740b40c9c880a0f3056b1c023df3b8d6c843b27
Admin <admin@Admins-MacBook-Pro-3.local>
2018-02-15 22:43:38 -0800

    added basic outline of user input page

---
+++ client/src/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+      return (
+        <div className="Home">
+            <a href="/getmood">Redirect to getmood</a>
+        </div>
+      );
+    }
+  }
+  
+  export default Home;
---
+++ client/src/auth/Logout.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Logout extends Component {
+  
+  render() {
+    return (
+      <h1>Logout Component</h1>
+    );
+  }
+}
+
+export default Logout;
