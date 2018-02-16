c287e296c9b2fc0c256124507cf2a93dd586561d
Admin <admin@Admins-MacBook-Pro-3.local>
2018-02-16 11:23:32 -0800

    passing emotion up to app.js

--- client/src/GetMood.js
+++ client/src/GetMood.js
@@ -7,29 +7,27 @@ class GetMood extends Component {
       super(props);
       this.state = {
         img:null,
-        txt:""
+        emotions:null
       }
     }
     handleFormSubmit = (e) =>{
       e.preventDefault();
-      axios({ 
-        method: 'POST', 
-        url: 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceId=true&returnFaceLandmarks=false&returnFaceAttributes=emotion', 
-        headers: {
-          "Content-Type":"application/octet-stream",
-          "Ocp-Apim-Subscription-Key": process.env.REACT_APP_API_KEY
-        }, 
-        data: this.state.img ,
-        processData: false
+        if(this.state.img !== null){
+          document.getElementsByClassName("Mood")[0].innerHTML = "Loading"; //Set loading wheel here
+        axios({ 
+          method: 'POST', 
+          url: 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceId=true&returnFaceLandmarks=false&returnFaceAttributes=emotion', 
+          headers: {
+            "Content-Type":"application/octet-stream",
+            "Ocp-Apim-Subscription-Key": process.env.REACT_APP_API_KEY
+          }, 
+          data: this.state.img ,
+          processData: false
 
-      }).then((response)=>{
-        console.log(response.data[0].faceAttributes.emotion);
-      })
-    }
-    handleTextChange = (e) =>{
-      this.setState({
-        txt:e.target.value
-      })
+        }).then((response)=>{
+          this.props.setEmotion(response.data[0].faceAttributes.emotion)
+        })
+      }
     }
     handleImgChange = (e) =>{
       /*Blob*/
@@ -82,8 +80,6 @@ class GetMood extends Component {
             <img src="https://d1o50x50snmhul.cloudfront.net/wp-content/uploads/2017/02/15180000/gettyimages-158781036-800x533.jpg" alt="Moodie Logo" className="mood-logo"/>
             <h2>Take a Picture</h2>
             <input onChange={this.handleImgChange} className="mood-img-input" name="img" type="file" accept="image/*" capture="camera" />
-            <h2>Write a few words about how you feel</h2>
-            <textarea onChange={this.handleTextChange} value={this.state.txt} cols="40" rows="5" className="mood-txt-input" name="txt" type="text"/>
             <input type="submit" className="mood-submit"/>
         </form>
       );
